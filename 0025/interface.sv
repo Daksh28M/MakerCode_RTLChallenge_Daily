e9b2d8e8805bb1255812136e85d14341aaacbd4d
Daksh28M <daksh6382@gmail.com>
2026-06-29 16:42:52 +0530

    solved q25-27

--- 0025/interface.sv
+++ 0025/interface.sv
@@ -16,5 +16,42 @@ module sync_fifo #(
 );
 
 // your implementation here
+reg [DATA_W-1:0] mem [DEPTH-1:0];
+reg [$clog2(DEPTH)-1:0] rd_ptr;
+reg [$clog2(DEPTH)-1:0] wr_ptr;
+reg [$clog2(DEPTH):0]cnt;
+
+reg [DATA_W-1:0] pop_reg;
+
+always_ff @(posedge clk or posedge reset) begin
+  if(reset) begin
+    // mem <= {DEPTH{'b0}};
+    rd_ptr <= 'b0;
+    wr_ptr <= 'b0;
+    cnt <= 'b0;
+    pop_reg <= 'b0;
+  end
+  else begin
+    if(push_i && !full_o) begin
+      mem[wr_ptr] <= push_data_i;
+      wr_ptr <= wr_ptr + 1'b1;
+      // cnt <= cnt + 1'b1;
+    end
+    if(pop_i && !empty_o) begin
+      // pop_reg <= mem[rd_ptr];
+      rd_ptr <= rd_ptr + 1'b1;
+      // cnt <= cnt - 1'b1;
+    end
+
+    cnt <= cnt + (push_i && !full_o) - (pop_i && !empty_o);
+    // if(push_i && pop_i && !full_o && !empty_o) begin
+      // cnt <= cnt;
+    // end
+  end
+end
+
+assign pop_data_o = mem[rd_ptr];
+assign full_o = (cnt == DEPTH); 
+assign empty_o = (cnt == 0);
 
 endmodule
