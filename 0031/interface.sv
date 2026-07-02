fb7e33c4b8a08283f37641537a7f4a37a64bd760
Daksh28M <daksh6382@gmail.com>
2026-07-02 12:49:13 +0530

    solved ques 31,32

--- 0031/interface.sv
+++ 0031/interface.sv
@@ -7,5 +7,18 @@ module johnson_counter #(
     output wire [WIDTH-1:0]        count_out
 );
 // your implementation here
+reg [WIDTH-1:0] count;
+
+always @(posedge clk) begin
+    if(reset) begin
+        count <= {WIDTH{1'b0}};
+    end
+    else if(enable) begin
+        count <= {count[WIDTH-2:0],~count[WIDTH-1]};
+    end
+    else count <= count;
+end
+
+assign count_out = count;
 
 endmodule
--- 0032/interface.sv
+++ 0032/interface.sv
@@ -7,5 +7,30 @@ module clock_divider #(
     output wire                    clk_out
 );
 // your implementation here
+localparam CNT_WIDTH = $clog2(DIVIDE_FACTOR);
 
+reg [CNT_WIDTH-1:0] counter;
+
+wire out1;
+
+reg out2;
+
+always_ff @(posedge clk_in) begin
+    if(reset) begin
+        counter <= {CNT_WIDTH{1'b0}};
+    end
+    else if(enable) begin
+        if(counter==DIVIDE_FACTOR-1) begin
+            counter <= {CNT_WIDTH{1'b0}};
+        end 
+        else counter <= counter + 1'b1;
+    end
+end
+
+always_ff @(negedge clk_in) begin
+    out2 <= out1;
+end
+
+assign out1 = counter[CNT_WIDTH-1];
+assign clk_out = out2 | out1;
 endmodule
